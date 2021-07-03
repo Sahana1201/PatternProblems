a2e4e5d4c86885cbc98772a5f96bde3fed2e1325
Sahana1201 <85381730+Sahana1201@users.noreply.github.com>
2021-07-03 18:19:29 +0530

    Add files via upload

---
+++ PrintingMiddlehalfLine.java
@@ -0,0 +1,25 @@
+package com.xworx.Apptitude.matrixPattern;
+
+public class PrintingMiddlehalfLine {
+
+	public static void main(String[] args) {
+		int n = 3;
+		for (int j = 1; j <= n; j++) {
+			for (int i = 1; i <= n; i++) {
+				if (i == 3) {
+					System.out.print(j + "" + i + " ");
+				}
+				else if (j == 3) {
+					System.out.print(j + "" + i + " ");
+				} else {
+					System.out.print("   ");
+				}
+			}
+
+			System.out.println();
+		}
+
+	}
+
+	}
+
